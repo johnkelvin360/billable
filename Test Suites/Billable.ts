e357d7c7b2b7419c21e1f026f96efe4e40b4e831
johnkelvin360 <66466189+johnkelvin360@users.noreply.github.com>
2021-05-19 17:27:43 +0100

    Test Scripts for Billable

---
+++ Test Suites/Billable.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Billable</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>9bf55c62-dbf7-4e3f-b801-a2f43ceabc39</testSuiteGuid>
+   <testCaseLink>
+      <guid>fb7141a6-50bf-423a-be8b-410dfcc819fa</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Install and Launch</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a6501743-29f6-4cdf-b802-b0711828f52f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Launch and Parse File Path</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
